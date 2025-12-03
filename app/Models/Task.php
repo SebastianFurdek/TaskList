ea3952371c1a1dfb819d62c8acf15d2777e0ff70
Sebas <sebastiann.furdek@gmail.com>
2025-12-03 12:24:13 +0100

    pridanie migráciía modelov

---
+++ app/Models/Task.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = ['title','description','due_date','completed','project_id','user_id'];
+    protected $casts = ['completed' => 'boolean', 'due_date' => 'date'];
+    public function user() { return $this->belongsTo(User::class); }
+    public function project() { return $this->belongsTo(Project::class); }
+    public function categories() { return $this->belongsToMany(Category::class); }
+}
