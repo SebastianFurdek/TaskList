d23a7aa3e120dab41507062317a72338edee819f
Sebas <sebastiann.furdek@gmail.com>
2026-01-18 17:54:36 +0100

    uprava vzhladu uloh

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Tasks Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card border-left-primary shadow h-100 py-2 dashboard-card">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
@@ -32,7 +32,7 @@
 
         <!-- Projects Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
+            <div class="card border-left-success shadow h-100 py-2 dashboard-card">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
@@ -52,7 +52,7 @@
 
         <!-- completed tasks Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
+            <div class="card border-left-info shadow h-100 py-2 dashboard-card">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
