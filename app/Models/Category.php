1e82c8635c58c7ce5cf3ac9bd16b333adfb2fbd1
Sebas <sebastiann.furdek@gmail.com>
2026-01-18 15:00:17 +0100

    uprava modelu Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name','color'];
+    protected $fillable = ['name','color', 'user_id'];
+
+    //kategoria patri jednemu uzivatelovi
+    public function user() { return $this->belongsTo(User::class); }
+    //M:n vztah kategoria ma viac uloh
     public function tasks() { return $this->belongsToMany(Task::class); }
 }
